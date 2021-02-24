ec52d20ab1ac3e0a33421435a9268bfa8a044733
Julian-Leon <e.leonc@uniandes.edu.co>
2021-02-24 00:48:57 -0500

    Base para inciar a trabajar

--- App/view.py
+++ App/view.py
@@ -44,7 +44,7 @@ def printMenu():
     print("5- Video con más likes")
 
 
-def initCatalog():
+def initCatalog(election_type):#intento pasar la info tomada del imput en el menú.
     """inicializa el catalogo de videos"""
     return controller.initCatalog()
 
@@ -98,7 +98,9 @@ while True:
         print("Cargando información de los archivos ....")
         print("1-LINKED_LIST")
         print("2-ARRAY_LIST")
-        input("Su eleccion es... ")
+        tipo=input("Su eleccion es... ")
+        election_type=controller.initCatalog(tipo)
+        initCatalog(election_type)
 
     elif int(inputs[0]) == 2:
         country=input("Ingrese el país: ")
